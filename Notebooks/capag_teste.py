daac65a82e3427b859186d70eb980173d9828d2d
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-08 18:12:12 -0300

    Update capag_teste.py

--- Notebooks/capag_teste.py
+++ Notebooks/capag_teste.py
@@ -117,7 +117,7 @@ elif select_event == 'Análise das Variáveis Quantitativas':
         with col1:
             st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Pib Percentual'</h1>", unsafe_allow_html=True)
             fig, ax = plt.subplots()
-            fig = ax.boxplot(df['PIB_PERC'])
+            fig = ax.boxplot(df['PIB_PERC'], clear_figure = False)
             st.pyplot(fig)
         with col2: 
             st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
