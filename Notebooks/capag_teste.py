9a52b89288beff9699d55f7cfff61a033364e750
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-08 14:15:05 -0300

    Update capag_teste.py

--- Notebooks/capag_teste.py
+++ Notebooks/capag_teste.py
@@ -22,7 +22,8 @@ pd.options.display.float_format = "{:,.2f}".format
 st.title('Classificação da Capacidade de Pagamento dos Municípios brasileiros com base em dados geoeconômicos')
 
 st.markdown('Uma nova visão sobre a capacidade de pagamento de nossos municípios')
-# https://github.com/MarioPrado1148/Capag/blob/main/Datasets/df_streamlit_com_previsao.csv',error_bad_lines=False
+
+@st.cache(persist=True)
 df = pd.read_csv('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Datasets/df_streamlit_com_previsao.csv', sep = ';', index_col = 0)
 
 # Preparação do sidebar (barra lateral)
@@ -39,6 +40,21 @@ select = st.sidebar.selectbox('município', lista_municipios)
 df = df.set_index('Município')
 df['CLASS_CAPAG_real'] = df['CLASS_CAPAG_real'].astype('Int64')
 df_municipio = df[df.index==(select)]
-
-
 st.write(df_municipio)
+
+if st.sidebar.checkbox("Análise Exploratória de Dados", True, key=2):
+    tipo_variaveis = ['Variáveis Categóricas', 'Variáveis Quantitativas']
+    select = st.sidebar.selectbox('Tipo de Variável', tipo_variaveis)
+
+     
+if select == 'Variáveis Categóricas':
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header('Região')
+        sns.countplot(data = df, x = "Região")
+        st.pyplot() 
+    with col2: 
+        st.header('Reg_Metropolitana')
+        sns.countplot(data = df, x = "Reg_Metropolitana")
+        st.pyplot()
+
