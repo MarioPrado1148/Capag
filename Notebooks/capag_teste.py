eb0f73f47d25cb252a1437a61464788ead43f0b0
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-08 11:59:49 -0300

    Update capag_teste.py

--- Notebooks/capag_teste.py
+++ Notebooks/capag_teste.py
@@ -13,13 +13,5 @@ import numpy as np
 import seaborn as sns
 import matplotlib as plt
 
-# Carregamento dos dados
-data_url =('D:/PremioTesouroNacional/df_streamlit_com_previsao.xlsx')
-#@st.cache(persist=True)
 
-#def load_data():
-data=pd.read_excel(data_url)
- #   return data
-
-#df =load_data()
 st.title('Classificação da Capacidade de Pagamento dos Municípios brasileiros com base em dados geoeconômicos')
