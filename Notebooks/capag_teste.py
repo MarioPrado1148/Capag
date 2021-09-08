853d0405725d32a1aa80dd8f9cc879e547da1b29
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-08 18:23:21 -0300

    Update capag_teste.py

--- Notebooks/capag_teste.py
+++ Notebooks/capag_teste.py
@@ -118,7 +118,7 @@ elif select_event == 'Análise das Variáveis Quantitativas':
         with col1:
             st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Pib Percentual'</h1>", unsafe_allow_html=True)
             fig = sns.boxplot(x = 'PIB_PERC', y = 'CLASS_CAPAG', data = df)
-            st.pyplot()
+            st.pyplot(fig)
         with col2: 
             st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
             st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
