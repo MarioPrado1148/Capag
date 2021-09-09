8d399d9f38c2496f7e41fb45dcb535658dc8a7b7
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-09 08:14:20 -0300

    Update capag.py

--- Notebooks/capag.py
+++ Notebooks/capag.py
@@ -3,6 +3,8 @@
 
 # Referências:
 # https://www.analyticsvidhya.com/blog/2020/10/create-interactive-dashboards-with-streamlit-and-python/
+# https://towardsdatascience.com/build-your-first-data-visualization-web-app-in-python-using-streamlit-37e4c83a85db
+# https://www.analyticsvidhya.com/blog/2020/12/deploying-machine-learning-models-using-streamlit-an-introductory-guide-to-model-deployment/
 
 # !pip install streamlit --upgrade
 
@@ -12,6 +14,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib as plt
+import plotly.express as px
+import math
 
 # Configuração da página
 st.set_page_config(layout="wide")
@@ -20,11 +24,11 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 pd.options.display.float_format = "{:,.2f}".format
 
 # Carregamento dos dados
-data_url =('https://github.com/MarioPrado1148/Capag/blob/main/Datasets/df_streamlit_com_previsao.csv')
+data_url =('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Datasets/df_streamlit_com_previsao.csv')
 @st.cache(persist=True)
 
 def load_data():
-    data=pd.read_csv(data_url)
+    data=pd.read_csv(data_url, sep = ';', index_col = 0)
     return data
 
 df =load_data()
@@ -33,26 +37,158 @@ df =load_data()
 st.title('Classificação da Capacidade de Pagamento dos Municípios brasileiros com base em dados geoeconômicos')
 
 # Preparação do sidebar (barra lateral)
-st.sidebar.title('Seletor de Visualizações')
-st.sidebar.markdown('Selecione os gráficos/dataframes que deseja visualizar')
+st.sidebar.title('Seletor de Análises e Gráficos')
+#st.sidebar.markdown('Que informações você gostaria de visualizar?')
 
-st.sidebar.checkbox("Visualizar análises por município", True, key=1)
-lista_municipios = pd.Series(list(set(df['Município']))).sort_values()
-select = st.sidebar.selectbox('município', lista_municipios)
+lista_eventos = ['Contextualização','Análise das Variáveis Categóricas','Análise das Variáveis Quantitativas', 'Dataframe completo', 'Visão por Município']
+select_event = st.sidebar.selectbox('Que informações você gostaria de visualizar?', lista_eventos)
 
-#st.table(df)
+if select_event == 'Contextualização':
+    st.markdown('A análise da capacidade de pagamento (Classificação CAPAG) apura a situação fiscal dos entes subnacionais que querem \
+		contrair novos empréstimos com garantia da União. Esta classificação é um indicador de saúde econômica e fiscal dos municípios.')
+    st.markdown('O intuito da CAPAG é apresentar de forma simples e transparente se um novo endividamento representa risco de crédito para o Tesouro Nacional.\
+		Assim, um ente bem avaliado pelo Tesouro Nacional poderá acessar empréstimos com juros mais baixos, por contar com a União como seu garantidor.\
+		Se, por algum motivo, o ente não puder honrar o pagamento da dívida, a União é quem terá que assumir o pagamento. A metodologia da CAPAG,\
+		fruto de um aprimoramento da metodologia já usada anteriormente, foi elaborada pelo Tesouro Nacional, com apoio do Banco Mundial. O cálculo da CAPAG, \
+		além de sintetizar essa situação fiscal em uma simples nota, possibilita a comparação entre os entes, com base em metodologia e informações conhecidas\
+		e padronizadas. Corresponde a um serviço similar ao prestado pelas agências de classificação de risco de crédito. A própria STN passou a calcular e divulgar\
+		as notas de modo regular, independentemente da existência de pedidos de aval ou garantia para operações de crédito. Enfim, trata-se de uma informação bastante útil\
+		para os que buscam melhor compreender as finanças dos entes subnacionais.')
+    st.markdown('Obter uma boa classificação da CAPAG é muito importante para os municípios, pois:')
+    st.markdown('1) ao obter este reconhecimento por parte do Tesouro Nacional, o município obtém a garantia da União para contrair empréstimos; tendo a União como garantidor, o município pode obter melhores condições de financiamento.')
+    st.markdown('2) licitações a preços mais vantajosos: empresas privadas podem utilizar a classificação CAPAG como um dos indicadores na tomada de decisão sobre participar ou não de um certame. O município, tendo uma boa capacidade de pagamento, pode atrair mais participantes para suas licitações e, consequentemente, propostas mais vantajosas economicamente.')
+    st.markdown('3) a boa classificação de risco facilita a atração de empreendimentos e de investimentos privados para o município, pois reflete a boa saúde econômica e fiscal do ente. A classificação CAPAG pode ser um indicador a ser considerado pelos empresários e pelos investidores na tomada de decisão sobre onde abrir negócios ou investir dinheiro.')
+    st.markdown('O objetivo da CAPAG é apresentar, de forma simples e transparente, os dados sobre a saúde financeira e fiscal dos municípios e é, conforme descrito acima, de grande interesse público e privado.')
 
-#df = df.drop(['Unnamed'],axis = 1)
-df = df.set_index('Município')
-df['CLASS_CAPAG_real'] = df['CLASS_CAPAG_real'].astype('Int64')
-df_municipio = df[df.index==(select)]
-
-
-st.write(df_municipio)
     
+# Análise Exploratória de dados das Variáveis Categóricas
+elif select_event == 'Análise das Variáveis Categóricas':
+    select_radio = st.sidebar.radio('Selecione a variável',['Região','Região Metropolitana','Unidade da Federação', 'Mun_Reg_Geog_Imediata','Hierarquia Urbana','Região Rural'])
+    if select_radio == 'Região':
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Região'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "Região")
+            st.pyplot()
+            
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
+    elif select_radio == 'Região Metropolitana':
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Região Metropolitana'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "Reg_Metropolitana")
+            st.pyplot()
+            
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
+    elif select_radio == 'Unidade da Federação':
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Unidade da Federação'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "UF")
+            st.pyplot()
+            
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
     
+    elif select_radio == 'Mun_Reg_Geog_Imediata':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Mun_Reg_Geog_Imediata'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "Mun_Reg_Geog_Imediata")
+            st.pyplot()
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
+    elif select_radio == 'Hierarquia Urbana':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Hierarquia Urbana'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "Hierarquia_Urbana")
+            st.pyplot()
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
     
-
-
-
+    elif select_radio == 'Região Rural':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Região Rural'</h1>", unsafe_allow_html=True)
+            sns.countplot(data = df, x = "Região_rural")
+            st.pyplot()
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
+                  
+# Análise Exploratória de dados das Variáveis Quantitativas                 
+elif select_event == 'Análise das Variáveis Quantitativas':
+    select_radio = st.sidebar.radio('Selecione a variável',['PIB percentual', 'VAB_Agricultura/Total','VAB_Indústria/Total','VAB_Serviço/Total','VAB_Adm/Total'])
+    if select_radio == 'PIB percentual':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Pib Percentual'</h1>", unsafe_allow_html=True)
+            fig = px.box(df, y = 'PIB_PERC')
+            fig.update_layout(height=400, width = 400)
+            st.plotly_chart(fig,height=400)        
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
     
+    elif select_radio == 'VAB_Agricultura/Total':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'VAB_Agricultura/Total'</h1>", unsafe_allow_html=True)
+            fig = px.box(df, y = 'VAB_Agricultura/Total')
+            st.plotly_chart(fig)        
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
+   
+    elif select_radio == 'VAB_Indústria/Total':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'VAB_Indústria/Total'</h1>", unsafe_allow_html=True)
+            fig = px.box(df, y = 'VAB_Indústria/Total')
+            st.plotly_chart(fig)        
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
+    
+    elif select_radio == 'VAB_Serviço/Total':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'VAB_Serviço/Total'</h1>", unsafe_allow_html=True)
+            fig = px.box(df, y = 'VAB_Serviço/Total')
+            st.plotly_chart(fig)        
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
+    
+    elif select_radio == 'VAB_Adm/Total':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'VAB_Adm/Total'</h1>", unsafe_allow_html=True)
+            fig = px.box(df, y = 'VAB_Adm/Total')
+            st.plotly_chart(fig)        
+        with col2: 
+            st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
+            st.markdown('Verifica-se que há maior quantidade de municípios na Região Nordeste, a qual é seguida de perto pela Região Sudeste')
+
+# Apresentação do Dataframe completo
+elif select_event == 'Dataframe completo':
+    st.table(df)
+else:
+  # st.sidebar.checkbox("Visualizar análises por município", True, key=1)
+    lista_municipios = pd.Series(list(set(df['Município']))).sort_values()
+    select = st.sidebar.selectbox('município', lista_municipios)
+    df = df.set_index('Município')
+    df['CLASS_CAPAG_real'] = df['CLASS_CAPAG_real'].astype('Int64')
+    df_municipio = df[df.index==(select)]
+    st.write(df_municipio)
