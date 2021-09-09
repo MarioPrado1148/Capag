4e240a61a0fd72b80450b33eb1d44300af570269
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-09 12:39:10 -0300

    Update capag.py

--- Notebooks/capag.py
+++ Notebooks/capag.py
@@ -100,7 +100,7 @@ elif select_event == 'Análise das Variáveis Categóricas':
             
         with col2: 
             st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
-            st.markdown('Verifica-se que há maior parte dos municípios não faz parte de Regiões Metropolitanas.')
+            st.markdown('Verifica-se que a maior parte dos municípios não está localizada em Regiões Metropolitanas.')
     elif select_radio == 'Unidade da Federação':
         col1, col2 = st.columns(2)
         
