efce18e1e6ac23417835d8910b1f2bc48bf96246
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-08 21:32:41 -0300

    Update capag_teste.py

--- Notebooks/capag_teste.py
+++ Notebooks/capag_teste.py
@@ -115,7 +115,7 @@ elif select_event == 'Análise das Variáveis Categóricas':
 elif select_event == 'Análise das Variáveis Quantitativas':
     select_radio = st.sidebar.radio('Selecione a variável',['PIB percentual', 'VAB_Indústria/Total'])
     if select_radio == 'PIB percentual':
-        col1 = st.columns(1)
+        col1, col2 = st.columns(2)
         with col1:
             st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Pib Percentual'</h1>", unsafe_allow_html=True)
             fig = px.box(df, y = 'PIB_PERC')
