fe33d00dce155a313ad41e9d881f9eb69880519b
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-09 12:00:43 -0300

    Update capag.py

--- Notebooks/capag.py
+++ Notebooks/capag.py
@@ -50,7 +50,7 @@ if select_event == 'Apresentação':
 	st.write('teste')
 	#image = Image.open('sunrise.jpg')
 	url = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/carolina-munemasa-FYBJgygqCzM-unsplash_Ouro_Preto.jpg')
-	#response = requests.get(url)
+	response = requests.get(url)
 		
 elif select_event == 'Contextualização':
     st.markdown('A análise da capacidade de pagamento (Classificação CAPAG) apura a situação fiscal dos entes subnacionais que querem \
