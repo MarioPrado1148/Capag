443f63369ed460affa218e3b5d8338549761f7d9
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-09 12:55:45 -0300

    Update capag.py

--- Notebooks/capag.py
+++ Notebooks/capag.py
@@ -137,8 +137,7 @@ elif select_event == 'Análise das Variáveis Categóricas':
         with col1:
             st.markdown("<h1 style='text-align: center; color: blue;'>Variável 'Região Rural'</h1>", unsafe_allow_html=True)
             ax = sns.countplot(data = df, x = "Região_rural")
-	    ax.set_xticklabels(ax.get_xticklabels(),rotation = 30)
-            st.pyplot()
+	    st.pyplot()
         with col2: 
             st.markdown("<h1 style='text-align: center; color: blue;'>Análise</h1>", unsafe_allow_html=True)
             st.markdown('Verifica-se que a maior parte dos municípios não faz parte de Regiões Metropolitanas.')
