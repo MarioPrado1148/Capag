4a4aaf630f96aa412fa0665c45facf478f519c1c
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-10 11:49:33 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -61,7 +61,9 @@ if radio == 'Visão cidadão':
 		st.text('Fotografia de Ouro Preto (MG). Autoria de Carolina Munemasa.')
 		st.text('Fonte: https://unsplash.com/s/photos/ouro-preto. Consultado em 09/09/2021')
 	elif select_event_cidadao == 'Objetivos':
-		st.write('objetivos')	
+		st.markdown('Neste trabalho, utilizamos um modelo de aprendizado de máquina denominado 'Gradient Boosting' com o objetivo de, utilizando as informações disponíveis \
+		sobre os municípios brasileiros, estimar a capacidade de pagamento (Capag) dos municípios e compará-las com a Capag constante do site da Secretaria do Tesouro Nacional\
+		(STN), quando disponível.')	
 	elif select_event_cidadao == 'Contextualização':
 		st.markdown('A análise da capacidade de pagamento (Classificação CAPAG) apura a situação fiscal dos entes subnacionais que querem \
 		contrair novos empréstimos com garantia da União. Esta classificação é um indicador de saúde econômica e fiscal dos municípios.')
