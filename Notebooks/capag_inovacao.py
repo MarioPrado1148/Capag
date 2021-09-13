8773a25d76eff589e3880d38a3cfd623493b50b0
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-13 18:30:07 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -36,8 +36,9 @@ def load_data():
 df =load_data()
 
 # Página Principal
-#texto = 'Classificação da capacidade de pagamento dos municípios brasileiros com base em dados geoeconômicos'
-st.title('Classificação da capacidade de pagamento dos municípios brasileiros com base em dados geoeconômicos')
+texto = 'Classificação da capacidade de pagamento dos municípios brasileiros com base em dados geoeconômicos'
+st.write(<title>{texto}</title>)
+#st.title('Classificação da capacidade de pagamento dos municípios brasileiros com base em dados geoeconômicos')
 #st.title(f"<h2 style='text-align: justify;'><b>{texto}</b></h2>", unsafe_allow_html=True)
 
 st.sidebar.title('Estimação da Capag com Ciências de Dados')
