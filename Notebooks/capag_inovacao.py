b0516efeb7eb29a3c674c01c60b46356815e9aa0
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-14 13:29:38 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -229,7 +229,8 @@ if radio == 'Visão cientista de dados':
 	elif select_event_cientista == 'Matriz de correlação':
 		st.write('Matriz de correlação')
 		df_quanti = df[['PIB_PERC', 'VAB_Indústria/Total', 'VAB_Serviço/Total', 'VAB_Adm/Total','VAB_Agricultura/Total']]
-		st.write(df_quanti.corr())
+		st.dataframe(df_quanti)
+		#st.write(df_quanti.corr())
 		#sns.set_context("notebook", font_scale=1.0, rc={"lines.linewidth": 2.5})
 		#plt.figure(figsize=(35,30))
 		#mask = np.zeros_like(corr)
