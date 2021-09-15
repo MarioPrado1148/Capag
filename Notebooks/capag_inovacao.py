1300acaca90d74115f3bdf5a295c813ba9ebbdd5
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-15 11:33:56 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -230,6 +230,10 @@ if radio == 'Visão cientista de dados':
 		url = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/corr_matrix3.jpg')
 		response = requests.get(url)
 		img = Image.open(BytesIO(response.content))
+		st.markdown('A matriz de correlação expressa a relação entre as variáveis quantitativas.')
+		st.markdown('O valor mais alto de correlação, em módulo, refere-se à correlação entre as variáveis 'VAB_Adm/Total' e 'PIB_PERC'. O sinal negativo indica que quando uma aumenta, a outra diminui.)
+		st.markdown('Ainda que seja considerada uma correlação forte, decidimos manter ambas as variáveis no modelo.')	    
+			    
 		st.image(img)
 		
 		
