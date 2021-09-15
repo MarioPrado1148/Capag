298178bc19d0ac7a6ebfaa9a93816d06e100e862
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-15 10:48:55 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -228,7 +228,7 @@ if radio == 'Visão cientista de dados':
 		st.write('Análise bivariada')
 	elif select_event_cientista == 'Matriz de correlação':
 		df_quanti = df[['PIB_PERC', 'VAB_Indústria/Total', 'VAB_Serviço/Total', 'VAB_Adm/Total','VAB_Agricultura/Total']]
-		fig = sns.heatmap(df_quanti.corr(), ax=ax)
+		fig = sns.heatmap(df_quanti.corr())
 		st.write(fig)
 		
 	elif select_event_cientista == 'Dataframe completo':
