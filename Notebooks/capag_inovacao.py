94e1faae43f6ffbc1c7fb33df960702980362769
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-16 19:27:34 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -54,12 +54,6 @@ if radio == 'Visão cidadão':
 	select_event_cidadao = st.sidebar.selectbox(
 		'Que informações você gostaria de visualizar?',
 		lista_eventos_cidadao)
-	# Imagem
-	#url2 = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/riodejaneiro.jpg')
-	url2 = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/digitalmarketing.jpg')
-	response = requests.get(url2)
-	img2 = Image.open(BytesIO(response.content))
-	st.sidebar.image(img2)
 		
 	
 	if select_event_cidadao == 'Apresentação':
@@ -76,7 +70,11 @@ if radio == 'Visão cidadão':
 		st.markdown(texto1)
 		st.markdown('Esse modelo utilizou apenas as variáveis geoeconômicas para estimar a Capag dos municípios para os quais esta não foi calculada pelo órgão classificador.')
 		st.markdown('Entende-se que é uma solução embrionária. Possuindo o órgão classificador outras variáveis úteis ao cálculo, as features utilizadas na solução proposta poderiam ser utilizadas em caráter suplementar, subsidiário na estimação.')
-		
+		# Imagem
+		url2 = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/digitalmarketing.jpg')
+		response = requests.get(url2)
+		img2 = Image.open(BytesIO(response.content))
+		st.sidebar.image(img2)
 	elif select_event_cidadao == 'Capag - Visão detalhada':
 		# st.sidebar.checkbox("Visualizar análises por município", True, key=1)
 		lista_municipios = pd.Series(list(set(df['Município']))).sort_values()
