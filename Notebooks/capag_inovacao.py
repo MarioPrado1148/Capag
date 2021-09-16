255b418f1476d5ee3acc3b8e530c3d248566b5d4
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-16 15:27:17 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -124,8 +124,8 @@ if radio == 'Visão cientista de dados':
 	if select_event_cientista == 'Coleta de Dados':
 		st.markdown('Foram utilizados 2 datasets neste trabalho:')
 		st.markdown('1 - Capacidade de Pagamento (CAPAG) dos Municípios')
-		st.text('Arquivo com 5569 linhas e 11 colunas, disponível no formato csv.')
-		st.text('Os dados foram coletados no website da Transparência do Tesouro Nacional 1, em janeiro de 2021.')
+		st.text('Arquivo com 5569 linhas e 19 colunas, disponível no formato csv.')
+		st.text('Os dados foram coletados no website da Transparência do Tesouro Nacional, em 16 de setembro de 2021.')
 		st.text('Link: http://www.tesourotransparente.gov.br/ckan/dataset/capag-municipios')
 		st.markdown('2.	PIB e outros indicadores econômicos')
 		st.text('Arquivo com 50115 linhas e 43 colunas, disponível no formato xls.')
