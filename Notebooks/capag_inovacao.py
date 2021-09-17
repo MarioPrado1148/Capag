075e53b80e1c5cb8fac699b38283b174b8ad931e
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-16 23:00:16 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -57,7 +57,8 @@ import pickle
 #model ='https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Models/modelo_randomizedsearch_xgboost_capag.pkl'
 #modelo_treinado = load_model(model)
 #modelo_treinado = pickle.load(open('modelo_randomizedsearch_xgboost_capag.pkl', 'rb'))
-modelo_treinado = st.file_uploader('modelo_randomizedsearch_xgboost_capag.pkl')
+pickle_file = open("modelo_randomizedsearch_xgboost_capag.pkl",“rb”)
+modelo_treinado = pickle.load(pickle_file)
 X, y = df.drop(['Capag'], axis = 1), df['Capag']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state = 42)
 predicao = modelo_treinado.predict(X)
