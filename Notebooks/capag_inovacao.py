f1069b0c8240aa5bffec90e6526170375cf98ea2
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-09-17 17:24:02 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -333,7 +333,7 @@ if radio == 'Visão cientista de dados':
 			with col2: 
 				st.header('Análise') 			
 				st.markdown('Verifica-se que a mediana do PIB dos municípios com Capag classes A e B (representadas pelo valor 0) é superior à mediana dos municípios com Capag  nas classe C e D (representadas pelo valor 1).');
-				st.markdown('Há mais outliers superiores (valores muito grandes, que se destacam dos demais), para os municípios das classes A e B (representadas pelo valor 0).')
+				st.markdown('Há mais outliers superiores (valores muito grandes, que se destacam dos demais), para os municípios das classes A e B.')
 		if select_radio_bivariada == 'Capag x VAB_Agricultura/Total':
 			col1, col2 = st.columns(2)
 			with col1:
@@ -380,7 +380,7 @@ if radio == 'Visão cientista de dados':
 				st.image(img6)
 			with col2: 
 				st.header('Análise') 			
-				st.markdown('Observa-se que a mediana do VAB_Adm/Total dos municípios com Capag classes C e D (representadas pelo valor 1) é superior à mediana dos municípios com Capag  nas classe A e B.')
+				st.markdown('Observa-se que a mediana do VAB_Adm/Total dos municípios com Capag classes C e D (representadas pelo valor 1) é superior à mediana dos municípios com Capag  nas classe A e B  (representadas pelo valor 0).')
 				st.markdown('Há mais outliers superiores (valores muito grandes, que se destacam dos demais), para os municípios das classes C e D.')
 
 		
