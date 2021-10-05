1201e786e21e94fdd84ed0bbe86c278fc0e1c852
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-10-05 16:24:43 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -30,11 +30,12 @@ pd.options.display.float_format = "{:,.2f}".format
 data_url =('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Dataset/df_streamlit_com_previsao5.csv')
 @st.cache(persist=True)
 
-def load_data():
-    data=pd.read_csv(data_url, sep = ',', index_col = 0)
-    return data
+#def load_data():
+ #   data=pd.read_csv(data_url, sep = ',', index_col = 0)
+  #  return data
 
-df =load_data()
+#df =load_data()
+df = pd.read_csv(data_url, sep = ',', index_col = 0)
 
 ########################################################################################
 # Carregamento dos dados
@@ -42,11 +43,12 @@ df =load_data()
 data_url_resumido =('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Dataset/df_streamlit_com_previsao_resumido6.csv')
 @st.cache(persist=True)
 
-def load_data_resumido():
-    data_resumido=pd.read_csv(data_url_resumido, sep = ',', index_col = 0)
-    return data_resumido
+#def load_data_resumido():
+ #   data_resumido=pd.read_csv(data_url_resumido, sep = ',', index_col = 0)
+  #  return data_resumido
 
-df_resumido =load_data_resumido()
+#df_resumido =load_data_resumido()
+df_resumido = pd.read_csv(data_url_resumido, sep = ',', index_col = 0)
 
 ###########################################################################################
 #import pickle
