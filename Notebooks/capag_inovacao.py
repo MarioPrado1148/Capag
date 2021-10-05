68e24c1adc726b26a93b973017079bc4af2566a7
Mario Prado <41242599+MarioPrado1148@users.noreply.github.com>
2021-10-05 18:12:27 -0300

    Update capag_inovacao.py

--- Notebooks/capag_inovacao.py
+++ Notebooks/capag_inovacao.py
@@ -396,7 +396,7 @@ if radio == 'Visão cientista de dados':
 	elif select_event_cientista == 'Métricas de avaliação do modelo':
 		select_radio_metricas = st.sidebar.radio('Selecione as variáveis',['F1 score - treino', 'F1 score - teste'])
 		if select_radio_metricas == 'F1 score - treino':
-		col1 = st.columns(1)
+			col1,col2 = st.columns(2)
 		with col1:
 			st.header('Variável PIB_per_capita')
 			url = ('https://raw.githubusercontent.com/MarioPrado1148/Capag/main/Images/boxplot_PIB_per_capita.jpg')
